1a18e93148d52dcab9cd9fe0a853fd0ae45fca03
藤中秋輔 <keitai54@gmail.com>
2019-05-01 17:43:32 +0900

    test app for machine learning

---
+++ app/tensorflow_test.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+
+# Step 1. Prepare data
+hello_data = 'Hello '
+world_data = 'World!'
+
+
+# Step 2. Define operation
+hello = tf.constant(hello_data)
+world = tf.constant(world_data)
+concat = hello + world
+
+
+# Step 3. Run operation
+session = tf.Session()
+print(session.run(concat))
+
+print(dir(tf.app))
+# print(help(tf.app))
